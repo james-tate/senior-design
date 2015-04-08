ce4cf8dcf1f6607a394c4c7026aef077e3bb42df
jld563 <jld563@msstate.edu>
2015-04-08 14:09:04 -0500

    Added results being outputted to a .txt file

--- camera system for Pi/detectLandingZone_pi.py
+++ camera system for Pi/detectLandingZone_pi.py
@@ -23,32 +23,43 @@ start_time = time.time()
 # -----------------------------------------
 
 # Set the path to the image directory
+# path = r'C:\\Users\\Josh\\Documents\\Senior Design\\images\\*.jpg'
+
 path = r'/home/pi/IPS/*.jpg'
 
 def detectLandingZone():
-    wd = os.getcwd()
+    minReq = 5
+    counter = 0
     truthArr = []
+    resultsFile = open('IPSresults.txt','w+')
+
     # Loop through all images in the directory
     for imName in glob.glob(path):
-	print 'Loading the image: %s'%imName
+        if (counter >= minReq ):
+            resultsFile.write('The minimum requirements have been met, Landing now\n')
+            print 'Minimum requirements have been met'
+            break
+        else:
+            print 'Loading the image: %s'% imName.split('/')[4]
+            resultsFile.write('The image being processed is %s\n' % imName.split('/')[4])
         # Load the image
-        im = cv2.imread(imName)
+            im = cv2.imread(imName)
 
         # Define the boundaries
-        boundaries= [([0,0,128],[34,34,255])]
+            boundaries= [([0,0,128],[34,34,255])]
 
         # Loop over the boundaries
-	print 'Getting image mask'
-        for (lower,upper) in boundaries:
+            print 'Getting image mask'
+            for (lower,upper) in boundaries:
             # Create NumPy arrays for lower and upper boundaries
-            lower = np.array(lower, dtype='uint8')
-            upper = np.array(upper, dtype='uint8')
+                lower = np.array(lower, dtype='uint8')
+                upper = np.array(upper, dtype='uint8')
 
             # Find the color (red) within the specified boundaries and apply the mask
-            mask = cv2.inRange(im,lower,upper)
-            masked_im = cv2.bitwise_and(im,im,mask=mask)
-            maxPix = np.max(masked_im[:,:,2])
-            minPix = np.min(masked_im[:,:,2])
+                mask = cv2.inRange(im,lower,upper)
+                masked_im = cv2.bitwise_and(im,im,mask=mask)
+                maxPix = np.max(masked_im[:,:,2])
+                minPix = np.min(masked_im[:,:,2])
 
             #print 'Max for %s is %d \n Min for %s is %d' % (imName,maxPix,imName,minPix)
 
@@ -59,16 +70,30 @@ def detectLandingZone():
 
 
             # Check to see if the max pixel value is what we are looking for
-            if (maxPix >= 128):
-                truthArr.append(1)
-            if (maxPix < 128):
-                truthArr.append(0)
+
+
+                if (maxPix >= 128):
+                    flag = True
+                    truthArr.append(1)
+                    counter = counter + 1
+                    resultsFile.write('Result: %s\n\n' % flag)
+                if (maxPix < 128):
+                    flag = False
+                    truthArr.append(0)
+                    resultsFile.write('Result: %s\n\n' % flag)
+
+
+
     print 'Writing truths to file'
     print truthArr
     truthFile = open('detectionTruth.txt','w')
-    for x in truthArr:
-        truthFile.write('%d, '%x)
-    truthFile.close()
+    if (counter >= minReq):
+	   truthFile.write('Pass\n')
+    else:
+		truthFile.write('Fail\n')
+#    for x in truthArr:
+#        truthFile.write('%d, '%x)
+#    truthFile.close()
 
 ##    truthFile = 'detectionTruth.csv'
 ##    with open(truthFile, "w") as csvfile:
